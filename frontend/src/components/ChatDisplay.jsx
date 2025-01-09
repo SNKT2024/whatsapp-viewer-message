36c0a89dd732582c5810678c29b8eb0d2fe016cb
Sanket Kumbhar <120247634+SNKT2024@users.noreply.github.com>
2025-01-09 17:09:42 +0530

    Update ChatDisplay.jsx

--- frontend/src/components/ChatDisplay.jsx
+++ frontend/src/components/ChatDisplay.jsx
@@ -39,7 +39,7 @@ function ChatDisplay({ currentUser, users }) {
 
   const scrollRef = useRef(null); // Reference for the scrollable container
   const isAtBottomRef = useRef(true); // Track if user is at the bottom of the chat
-  const BASE_URL = import.meta.env.BASE_URL;
+  const BASE_URL = "https://whatsapp-viewer-message-backend.onrender.com";
 
   /**
    * Fetch initial messages from backend on component mount or when selected user changes
